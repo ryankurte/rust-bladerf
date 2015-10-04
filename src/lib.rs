f56a1890979a81e9f2a87db5837cbc29fe85ab97
Ryan Kurte <ryankurte@gmail.com>
2015-10-04 17:06:30 +1300

    Added bindgen generated bindings, going to require much cleaning up

--- src/lib.rs
+++ src/lib.rs
@@ -3,43 +3,27 @@ extern crate libc;
 use libc::*;
 use std::*;
 
-pub static BLADERF_SERIAL_LENGTH: usize = 33;
+#[allow(dead_code, non_camel_case_types)]
+mod bladerf;
+use bladerf::*;
 
-#[repr(C)]
-pub enum bladerf_backend {
-    BLADERF_BACKEND_ANY,    
-    BLADERF_BACKEND_LINUX,  
-    BLADERF_BACKEND_LIBUSB, 
-    BLADERF_BACKEND_CYPRESS, 
-    BLADERF_BACKEND_DUMMY = 100,
-}
-
-#[repr(C)]
-pub struct bladerf_devinfo {
-    pub backend: bladerf_backend,
-    pub serial: [c_char; 33],
-    pub usb_bus: uint8_t,            
-    pub usb_addr: uint8_t,           
-    pub instance: libc::c_uint
-}
-
-impl fmt::Debug for bladerf_devinfo {
+impl fmt::Debug for Struct_bladerf_devinfo {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "serial: UNIMPLEMENTED, bus: {}, address: {})", self.usb_bus, self.usb_addr)
     }
 }
 
-#[link(name = "bladerf")]
-extern {
-	fn bladerf_get_device_list(devices: &*mut [bladerf_devinfo]) -> libc::c_int;
-	fn bladerf_free_device_list(devices: *mut [bladerf_devinfo]);
-    fn bladerf_set_usb_reset_on_open (enabled: bool);
-}
+//#[link(name = "bladerf")]
+//extern {
+//	fn bladerf_get_device_list(devices: &*mut [Struct_bladerf_devinfo]) -> libc::c_int;
+//	fn bladerf_free_device_list(devices: *mut [Struct_bladerf_devinfo]);
+//    fn bladerf_set_usb_reset_on_open (enabled: bool);
+//}
 
 pub fn get_device_list() -> Result<isize, isize> {
 
 	unsafe{ 
-		let devices: *mut [bladerf_devinfo] = mem::uninitialized();
+		let devices: *mut [Struct_bladerf_devinfo] = mem::uninitialized();
 
 		let n = bladerf_get_device_list(&devices) as isize;
 
@@ -64,7 +48,7 @@ pub fn get_device_list() -> Result<isize, isize> {
 
 pub fn set_usb_reset_on_open(enabled: bool) {
     unsafe{ 
-    	bladerf_set_usb_reset_on_open(enabled); 
+    	bladerf_set_usb_reset_on_open(enabled as uint8_t); 
     } 
 }
 
