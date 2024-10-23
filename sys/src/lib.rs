3d2b9ee0cd4fba930862ee6ced0d6b7f6250f1ac
Erik Fong <quantumerikfong@gmail.com>
2024-10-22 23:31:00 -0400

    Fix lint

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -12,8 +12,8 @@ impl bladerf_devinfo {
     /// Fetch serial string
     pub fn serial(&self) -> String {
         let mut s = [0u8; 33];
-        for i in 0..self.serial.len() {
-            s[i] = self.serial[i] as u8;
+        for (s_val, ser) in s.iter_mut().zip(self.serial) {
+            *s_val = ser as u8;
         }
 
         CStr::from_bytes_until_nul(&s)
